1db264a99d3073df20770e62973842deb36c2e4d
Viktor Kucher <ipzk221_kvo@student.ztu.edu.ua>
2022-11-06 20:04:56 +0200

    4-5 message changed

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp - 1");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp - 2");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp - 3");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp - 2");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp -1");
